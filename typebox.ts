3bab83c3dd1fc3adbe79147dc060307f0e7dddb4
Curtis Larson <quackware@gmail.com>
2023-03-30 18:23:28 -0400

    chore: export TypeBox Type as T

--- typebox.ts
+++ typebox.ts
@@ -1,3 +1,5 @@
+import { Type } from "https://esm.quack.id/@sinclair/typebox@0.26.8";
+export { Type as T };
 export * from "https://esm.quack.id/@sinclair/typebox@0.26.8";
 export * from "https://esm.quack.id/@sinclair/typebox@0.26.8/compiler";
 export * from "https://esm.quack.id/@sinclair/typebox@0.26.8/value";
