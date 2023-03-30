04be6522dc91b4fc9bd352a3e672eefcb5c02681
Curtis Larson <quackware@gmail.com>
2023-03-30 17:58:28 -0400

    feat: add typebox file, fix package.json description

---
+++ typebox.ts
@@ -0,0 +1,3 @@
+export * from "https://esm.quack.id/@sinclair/typebox@0.26.8";
+export * from "https://esm.quack.id/@sinclair/typebox@0.26.8/compiler";
+export * from "https://esm.quack.id/@sinclair/typebox@0.26.8/value";
